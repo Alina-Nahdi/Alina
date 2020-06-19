de88bed648aa751c49aae4370e791e22614748fd
Alina-Nahdi1 <nahdi.alina.khanam@gmAIL.COM>
2020-06-19 11:02:20 +0530

    Updates

---
+++ precache-manifest.a45eee053fcece1dbeb9c0344b2b3d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914b8c8cd9bbd425709764c5b700a6eb",
+    "url": "/Alina/index.html"
+  },
+  {
+    "revision": "796c218782af19cb42af",
+    "url": "/Alina/static/css/main.4c37ab83.chunk.css"
+  },
+  {
+    "revision": "43de80c296967606c7a2",
+    "url": "/Alina/static/js/2.6ad072e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Alina/static/js/2.6ad072e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796c218782af19cb42af",
+    "url": "/Alina/static/js/main.914f5636.chunk.js"
+  },
+  {
+    "revision": "db2402a34fc017c1f6b7",
+    "url": "/Alina/static/js/runtime-main.319ff1bd.js"
+  }
+]);
